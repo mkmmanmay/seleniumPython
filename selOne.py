2afff1a8407582456064f9fbc94ee313bdf4feb7
Manmay Kumar <mkmmanmay@gmail.com>
2017-06-19 23:17:28 +0530

    Launch google in first selenium program

---
+++ selOne.py
@@ -0,0 +1,8 @@
+import os
+
+from selenium import webdriver
+
+chromedriver = "C:/browserdrivers/chromedriver.exe"
+os.environ["webdriver.chrome.driver"] = chromedriver
+driver = webdriver.Chrome(chromedriver)
+driver.get("http://www.google.co.in")
