63595f99c746621e1fd3526841c248c1b683953d
Manmay Kumar <mkmmanmay@gmail.com>
2017-06-19 23:29:25 +0530

    maximize window & get title
    added maximize window code, and code to print the page's title

--- selOne.py
+++ selOne.py
@@ -6,3 +6,5 @@ chromedriver = "C:/browserdrivers/chromedriver.exe"
 os.environ["webdriver.chrome.driver"] = chromedriver
 driver = webdriver.Chrome(chromedriver)
 driver.get("http://www.google.co.in")
+driver.maximize_window()
+print(driver.title)
